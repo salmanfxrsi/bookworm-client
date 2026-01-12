b20e0af99fc2771bc6ac89cab7960c28bfee504d
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-12 12:54:01 +0600

    feat: delete nextauth login system for error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,17 +21,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
-        <Footer />
+          {children}
+          <Footer />
       </body>
     </html>
   );
