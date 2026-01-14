a3741a3a8c13dd337454295749ab2a1cd5755659
Salman Farsi <salmanfarsi9005@gmail.com>
2026-01-14 22:14:49 +0600

    feat: make all routes protected following requirements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import FeaturedBooksSection from "@/components/home/FeaturedBooksSection/FeaturedBooksSection";
 import GenresSection from "@/components/home/GenresSection";
 import HeroSection from "@/components/home/HeroSection";
@@ -6,6 +12,17 @@ import NewsletterSection from "@/components/home/NewsletterSection";
 import TopAuthorsSection from "@/components/home/TopAuthorsSection";
 
 export default function HomePage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/auth/login");
+    }
+  }, [status, router]);
+
+  if (status === "loading") return <div>Loading...</div>;
+
   return (
     <main className="flex flex-col min-h-screen bg-zinc-50 dark:bg-black font-sans">
       <HeroSection />
